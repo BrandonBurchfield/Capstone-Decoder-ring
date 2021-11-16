9fe38b96a6db4154730223d23c20600368647023
Brandon <bburch213@gmail.com>
2021-11-16 11:39:14 -0600

    commit

--- src/caesar.js
+++ src/caesar.js
@@ -2,14 +2,44 @@
 // as the index.html and test files rely on this setup to work properly.
 // Only add code (e.g., helper methods, variables, etc.) within the scope
 // of the anonymous function on line 6
-// test 2
 
 const caesarModule = (function () {
   // you can add any code you want within this function scope
 
   function caesar(input, shift, encode = true) {
     // your solution code here
+    if(!shift || shift === 0 || shift > 25 || shift < -25){
+      return false;
+    }
+    
+    if(!encode) shift *= -1;
+
+    let message = input.toLowerCase();
+    let final = "";
+
+    for(let text = 0; text < message.length; text++){
+      let letter = message[text];
+    }
+
+    if(letter.match(/[a-z]/)){
+
+      let code = message.charCodeAt(text) + shift;
+      if (code > 122){
+        code = code - 26;
+      }
+      if (code < 97){
+        code = code + 26;
+      }
+      let newLetter = String.fromCharCode(code);
+      final += newLetter;
+    } else {
+      final += letter;
+    }
   }
+  console.log(final);
+  return final;
+}
+
 
   return {
     caesar,
